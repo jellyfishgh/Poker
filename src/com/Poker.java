27d6a3724f83a2c72ccb7836d997305144caa0dc
jellyfishgh <1129088994@qq.com>
2017-09-24 01:46:45 +0800

    vscode debug config

--- src/com/Poker.java
+++ src/com/Poker.java
@@ -3,15 +3,15 @@ package com;
 import java.util.Scanner;
 
 public class Poker {
-
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		CardPlayer player;
 		String str;
-		while(true) {
+		while (true) {
 			System.out.println("请输入你的纸牌：");
 			str = scan.nextLine();
-			if(str.equals("exit"))break;
+			if (str.equals("exit"))
+				break;
 			player = new CardPlayer(str);
 			System.out.println(player.result());
 		}
